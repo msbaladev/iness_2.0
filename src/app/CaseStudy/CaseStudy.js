0ef48e9cc7db1127ad3276bf00d9b906f38ab2b9
msbaladev <balamurugan@innesssolutions.com>
2024-06-24 10:52:50 +0530

    case study responsive

--- src/app/CaseStudy/CaseStudy.js
+++ src/app/CaseStudy/CaseStudy.js
@@ -13,7 +13,7 @@ const CaseStudy = () => {
   const router = useRouter();
 
   return (
-    <div className="min-h-screen relative">
+    <div className="min-h-screen relative lg:bg-red-700">
       <h5 className="font-bold text absolute  text-3xl p-4 flex">
         <span className="mr-5">
           {" "}
@@ -21,33 +21,47 @@ const CaseStudy = () => {
         </span>
         CaseStudy
       </h5>
-      <div className=" grid grid-cols-2 place-content-center place-items-center h-screen ">
+      <div className=" grid grid-cols-2 place-content-center place-items-center min-h-screen md:grid-cols-1 sm:grid-cols-1 ">
         <div>
           <img
-            class=" w-96 h-[500px] rounded-t-full object-cover"
+            class=" xl:w-96 xl:h-[500px] lg:h-[400px] lg:w-80 rounded-t-full object-cover w-52 h-52"
             src="https://img.freepik.com/free-photo/stylish-handsome-indian-man-tshirt-pastel-wall_496169-1571.jpg"
             alt="Profile Image"
           />
         </div>
 
-        <div className="space-y-4">
-          <h1>MULTIPLATFORM</h1>
-          <h1 className="text-4xl font-bold">
+        <div className="space-y-4 sm:mt-2 sm:text-center md:text-center">
+          <h1 className="text-green-500">MULTIPLATFORM</h1>
+          <h1 className="xl:text-6xl lg:text-2xl font-bold text-xl">
             {" "}
             Hive brings all your video platforms together
           </h1>
-          <h6 className="text-xl">
+          <h6 className="text-xl lg:text-sm sm:text-sm">
             Put an end to disconnected communication tools.
             <br /> Hive supports the use of multiple video platforms,
             simultaneously.
           </h6>
 
+          <div className="sm:flex md:flex  justify-center">
+          <h3
+            onClick={() => router.push("case-study")}
+            className="flex items-center text-blue-500 cursor-pointer hover:text-blue-800  "
+          >
+            Read more{" "}
+            <span className="ml-2">
+              <IoArrowForwardOutline />
+            </span>
+          </h3>
+          </div>
+      
+
+          {/* 
           <button
             onClick={() => router.push("case-study")}
             className="bg-green-600 py-2 px-6 rounded-lg"
           >
             case study
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
